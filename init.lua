1a3a198706d1f88b55814671d8ebfb44940699bd
Yamin Nather <2001s.yn@gmail.com>
2025-01-09 19:24:47 +0530

    =change windows shell

--- init.lua
+++ init.lua
@@ -8,6 +8,10 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=0")
 
+if string.find(vim.loop.os_uname().sysname:lower(), "windows") then
+	vim.cmd("set shell=powershell")
+end
+
 require("custom_plugins.yank_on_delete_disabler").setup()
 
 require("config.lazy")
