19dd3d52d5e6f27a1c24f90471fa3b9d7d833505
Yamin Nather <2001s.yn@gmail.com>
2025-01-23 19:14:38 +0530

    =added yaml and bash to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ if not vim.g.vscode then
 		build = ":TSUpdate",
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "vim", "go", "rust", "kotlin", "java" },
+				ensure_installed = { "lua", "vim", "go", "rust", "kotlin", "java", "yaml", "bash" },
 				sync_install = "false",
 				highlight = { enable = true },
 				indent = { enable = false },
